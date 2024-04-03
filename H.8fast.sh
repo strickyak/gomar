2eb8e471bd0b72830bb34a8542a429f4812edf2d
Henry Strickland <strick@yak.net>
2024-04-02 17:28:10 -0700

    demo scripts H.8* updated

--- H.8fast.sh
+++ H.8fast.sh
@@ -1,3 +1,65 @@
 # works with pages 31 (level 1), 327 (level 2 40wide), 328 (level 2 80wide).
 
-- gorun --tags=coco3,level2,vdg,cocoio,gime gomar.go -disk ../build-frobio/results/OS9DISKS/NOS9_6809_L2_coco3_80d.dsk -rom_8000 ../toolshed/cocoroms/coco3.rom -internal_rom_listing ../toolshed/cocoroms/coco3.rom.list --cart ../build-frobio/axiom4-whole.rom -external_rom_listing /dev/null
+
+INKEY=--inkey='~~~~~~~~~~~~~~~~~~~~~~~~326
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~@
+'
+SHOW='--show_vdg_screen=0'
+
+case "$1" in
+	h1)
+		INKEY='--inkey_file=inkey/decb_many_dirs.txt'
+		SHOW='--show_vdg_screen=1'
+		;;
+	h2)
+		INKEY='--inkey_file=inkey/decb_many_saves.txt'
+		SHOW='--show_vdg_screen=1'
+		;;
+	g1)
+		INKEY='--inkey_file=inkey/pmode11screen10.txt'
+		SHOW='--show_vdg_screen=1'
+		;;
+	g2)
+		INKEY='--inkey_file=inkey/decb_try_clear.txt'
+		SHOW='--show_vdg_screen=1'
+		;;
+esac
+
+- gorun \
+  --tags=coco3,level2,vdg,cocoio,gime gomar.go \
+    -disk ../build-frobio/results/OS9DISKS/NOS9_6809_L2_coco3_80d.dsk \
+      -rom_8000 ../toolshed/cocoroms/coco3.rom \
+      -internal_rom_listing ../toolshed/cocoroms/coco3.rom.list \
+	  --cart ../build-frobio/axiom4-whole.rom \
+	  -external_rom_listing ../build-frobio/hdbdos.rom.list \
+		  "$INKEY" "$SHOW"   
+exit $?
+
+- gorun --tags=coco3,level2,trace,d,vdg,cocoio,gime gomar.go \
+  -disk ../build-frobio/results/OS9DISKS/NOS9_6809_L2_coco3_80d.dsk \
+    -rom_8000 ../toolshed/cocoroms/coco3.rom \
+      -internal_rom_listing ../toolshed/cocoroms/coco3.rom.list \
+        --cart ../build-frobio/axiom41.rom \
+	  -external_rom_listing ../build-frobio/hdbdos.rom.list \
+	    --borges /home/strick/borges/  \
+	      --trigger_pc=55944  \
+	        --trigger_count=3  \
+		    -t=10'000'000  \
+		  "$INKEY" "$SHOW"
+
+
+
+
+
+#
+#- gorun --tags=coco3,level2,trace,d,vdg,cocoio,gime gomar.go \
+#  -disk ../build-frobio/results/OS9DISKS/NOS9_6809_L2_coco3_80d.dsk \
+#    -rom_8000 ../toolshed/cocoroms/coco3.rom \
+#      -internal_rom_listing ../toolshed/cocoroms/coco3.rom.list \
+#        --cart ../build-frobio/axiom4-whole.rom \
+#	  -external_rom_listing /dev/null \
+#	    --borges /home/strick/borges/  \
+#	      --trigger_pc=55944  \
+#	        --trigger_count=3  \
+#		  "$INKEY" "$SHOW"   \
+#		  -t=1
--- H.8t1.sh
+++ H.8t1.sh
@@ -4,4 +4,21 @@
 # - gorun --tags=coco3,level2,trace,d,vdg,cocoio,gime gomar.go -disk ../build-frobio/results/OS9DISKS/NOS9_6809_L2_coco3_80d.dsk -rom_8000 ../toolshed/cocoroms/coco3.rom -internal_rom_listing ../toolshed/cocoroms/coco3.rom.list --cart ../build-frobio/axiom4-whole.rom -external_rom_listing /dev/null --borges /home/strick/borges/  --t=1
 
 # - gorun --tags=coco3,level2,trace,d,vdg,cocoio,gime gomar.go -disk ../build-frobio/results/OS9DISKS/NOS9_6809_L2_coco3_80d.dsk -rom_8000 ../toolshed/cocoroms/coco3.rom -internal_rom_listing ../toolshed/cocoroms/coco3.rom.list --cart ../build-frobio/axiom4-whole.rom -external_rom_listing /dev/null --borges /home/strick/borges/  --trigger_pc=0xDA88  --trigger_count=3
-- gorun --tags=coco3,level2,trace,d,vdg,cocoio,gime gomar.go -disk ../build-frobio/results/OS9DISKS/NOS9_6809_L2_coco3_80d.dsk -rom_8000 ../toolshed/cocoroms/coco3.rom -internal_rom_listing ../toolshed/cocoroms/coco3.rom.list --cart ../build-frobio/axiom4-whole.rom -external_rom_listing /dev/null --borges /home/strick/borges/  --t=1
+
+INKEY=--inkey='~~~~~~~~~~~~~~~~~~~~~~~~326
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~@
+'
+SHOW='--show_vdg_screen=0'
+
+case "$1" in
+	h1)
+		INKEY='--inkey_file=inkey/decb_many_dirs.txt'
+		SHOW='--show_vdg_screen=1'
+		;;
+	h2)
+		INKEY='--inkey_file=inkey/decb_many_saves.txt'
+		SHOW='--show_vdg_screen=1'
+		;;
+esac
+
+- gorun --tags=coco3,level2,trace,d,vdg,cocoio,gime gomar.go -disk ../build-frobio/results/OS9DISKS/NOS9_6809_L2_coco3_80d.dsk -rom_8000 ../toolshed/cocoroms/coco3.rom -internal_rom_listing ../toolshed/cocoroms/coco3.rom.list --cart ../build-frobio/axiom4-whole.rom -external_rom_listing /dev/null --borges /home/strick/borges/  --t=1  --trigger_count=3  "$INKEY" "$SHOW"
