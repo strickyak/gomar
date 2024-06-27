5fa673f1bd3b1fa81a2ed07bf65b04dd5948320a
Henry Strickland <strick@yak.net>
2024-06-27 11:28:10 -0400

    fix run script for new pizga structure

--- H.8v41t1.sh
+++ H.8v41t1.sh
@@ -5,4 +5,7 @@
 
 # - gorun --tags=coco3,level2,trace,d,vdg,cocoio,gime gomar.go -disk ../build-frobio/results/OS9DISKS/NOS9_6809_L2_coco3_80d.dsk -rom_8000 ../toolshed/cocoroms/coco3.rom -internal_rom_listing ../toolshed/cocoroms/coco3.rom.list --cart ../build-frobio/axiom4-whole.rom -external_rom_listing /dev/null --borges /home/strick/borges/  --trigger_pc=0xDA88  --trigger_count=3
 
-- gorun --tags=coco3,level2,trace,d,vdg,cocoio,gime gomar.go -disk ../build-frobio/results/OS9DISKS/NOS9_6809_L2_coco3_80d.dsk -rom_8000 ../toolshed/cocoroms/coco3.rom -internal_rom_listing ../toolshed/cocoroms/coco3.rom.list --cart ../frobio/frob3/axiom41/axiom41.rom -external_rom_listing ../frobio/frob3/axiom41/axiom41.list --borges /home/strick/borges/  --t=1
+BUILD=../build-frobio/
+INT=../build-frobio/pizga/Internal/
+
+- gorun --tags=coco3,level2,trace,d,vdg,cocoio,gime gomar.go -disk $INT/OS9DISKS/NOS9_6809_L2_coco3_80d.dsk -rom_8000 ../toolshed/cocoroms/coco3.rom -internal_rom_listing ../toolshed/cocoroms/coco3.rom.list --cart $BUILD/axiom41.rom -external_rom_listing $BUILD/axiom41.rom.list --borges /home/strick/borges/  --t=1 "$@"
