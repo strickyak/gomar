df2d9a9a9b89ab505ce6b261c1097f9980ae0351
Henry Strickland <strick@yak.net>
2025-02-08 17:03:51 -0500

    adding run-coco1-ecb-axiom41.sh

--- run-coco1-ecb-axiom41.sh
+++ run-coco1-ecb-axiom41.sh
@@ -1,5 +1,16 @@
 cd $(dirname $0)
 
+echo "
+  Suggestion:  in ../build-frobio do this:
+      make run-lemma LAN=127.0.0.1 FORCE=--force_page=98
+  or (if you configure your wired ethernet) this:
+      make run-lemma LAN=10.23.23.23 FORCE=--force_page=98
+
+  But first, if you are forcing page 98,
+      cd ~/coco-shelf/nekot-coco-microkernel/kernel
+      make
+" >&2
+
 cat ../toolshed/cocoroms/extbas11.rom ../toolshed/cocoroms/bas13.rom > /tmp/rom1
 cat ../toolshed/cocoroms/extbas11.rom.list ../toolshed/cocoroms/bas13.rom.list > /tmp/list1
 
@@ -7,5 +18,6 @@ set -x
 go run --tags=coco1,level1,vdg,cocoio, gomar.go \
     -rom_8000 /tmp/rom1 \
     -internal_rom_listing /tmp/list1  \
+    --cart ../build-frobio/axiom41.rom \
     --show_vdg_screen=1 --bracket_terminal \
     "$@"
