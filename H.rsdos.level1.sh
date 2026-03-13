b442f695772bcdc251dc6e256ead061a16ad18ea
Henry Strickland <strick@yak.net>
2026-03-12 18:08:40 -0700

    H.rsdos.level1. hints

---
+++ H.rsdos.level1.sh
@@ -0,0 +1,12 @@
+(
+  cat ../toolshed/cocoroms/extbas11.rom
+  cat ../toolshed/cocoroms/bas13.rom 
+) > /tmp/coco2rom
+
+(
+  cat ../toolshed/cocoroms/extbas11.rom.list
+  cat ../toolshed/cocoroms/bas13.rom.list
+) > /tmp/coco2rom.list
+
+
+- gorun --tags=coco1,level1,vdg gomar.go -rom_8000 /tmp/coco2rom -internal_rom_listing  /tmp/coco2rom.list   --cart ../toolshed/cocoroms/disk11.rom -external_rom_listing ../toolshed/cocoroms/disk11.rom.list
